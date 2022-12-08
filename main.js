0a2db171f1487e1303aa87b4eac7cf4872e5b4e9
jeong byeong keon <coluber37@gmail.com>
2022-12-08 18:02:33 +0900

    fisrt commit

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ let maintext = document.querySelector("h1")
 window.addEventListener('scroll',function(){
     let value = window.scrollY
     console.log("scrolly", value);
+    if(value>400){
+        
+    }
 })
